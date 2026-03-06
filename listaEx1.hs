caa45566a6817cafd504446ae02dbcf605db2ce3
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-03-05 23:49:56 -0300

    Add Haskell functions for calculations and main execution

---
+++ listaEx1.hs
@@ -0,0 +1,15 @@
+f1 :: Float -> Float
+f1 x = if x >= 0 then (x+4)/(x+2) else 2/x
+
+f2:: Int -> Int -> Int
+f2 x y = if x >= y then x + y else x - y
+
+f3 :: Int -> Int -> Int -> Int
+f3 x y z = if (x + y) > z then x + y + z else if (x + y) < z then x - y - z else 0
+
+main :: IO()
+
+main = do
+    print (f1 2)
+    print (f2 5 10)
+    print (f3 10 20 30)
