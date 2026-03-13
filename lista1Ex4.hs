7c974fb1d47d15c0b03b2b6b1d92eaf4df8dc3b5
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-03-13 18:11:06 -0300

    Fix invert function implementation and update main execution; add new file for additional exercises

--- lista1Ex4.hs
+++ lista1Ex4.hs
@@ -1,7 +1,7 @@
 invert::Int -> Int
 
-invert x =  x `mod` 10 (como eu junto?) invert(x `div` 10) --usando o mod ele sempre vai pegar o ultimo numero, e usando o div, sempre vai pegar o primeiro
+invert x =  (x `mod` 10)*10^1 + (x `div` 10)*10^0--usando o mod ele sempre vai pegar o ultimo numero, e usando o div, sempre vai pegar o primeiro
 
 main::IO()
 main = do
-    print(invert 14)
+    print(invert 54)
