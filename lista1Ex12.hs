84f56ef2f14574fa258546c0616de18283a590f4
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-04-17 01:16:37 -0300

    Add comments to clarify logic in allDifferent function and create new file lista1Ex13.hs

--- lista1Ex12.hs
+++ lista1Ex12.hs
@@ -5,3 +5,7 @@ allDifferent m n p
     |otherwise = False
 
 
+    --O errado é que ele podia cometer o erro de ver que m era diferente de n e p era diferente de n, mas nao comparar
+    -- o m com o p, o que poderia resultar num falso e falso o que retorna True mas ainda sim o m ser igual ao p
+    
+
