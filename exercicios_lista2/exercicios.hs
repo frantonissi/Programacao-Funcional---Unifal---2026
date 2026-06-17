6c4204c864c0640233c9b007b983156fa5008c31
frantonissi <francisco.tonissi@sou.unifal-mg.edu.br>
2026-06-17 19:00:03 -0300

    Add exercises for calculating squares and their sums in Haskell
    - Implemented auxiliary functions to generate squares of numbers up to a given integer.
    - Created a recursive function to sum a list of integers.
    - Improved the implementation of the square summation with a more concise version.
    - Added comments to clarify the purpose of each function.

---
+++ exercicios_lista2/exercicios.hs
@@ -0,0 +1,27 @@
+-- Exercicio 1 feito completamente sozinho --
+aux :: Int  -> [Int]
+aux x  = [(a*a)|a <- [1..x]]
+
+a = aux 100
+
+quadrado :: [Int] -> Int
+quadrado [] = 0
+quadrado (a:b) = a + quadrado b
+
+--Ex 1 um pouquinho melhor--
+aux1 :: Int  -> [Int]
+aux1 x  = [(a*a)|a <- [1..x]]
+
+soma :: [Int] -> Int
+soma [] = 0
+soma (x:xs) = x + soma xs
+
+quadrado1 :: Int -> Int
+quadrado1 x= soma(aux1 x)
+
+
+--Ex 1 perfeito--
+ex1 :: Int  -> Int
+ex1 x  = sum [a*a|a <- [1..x]]
+
+--Ex 2--
--- lista_semanal/listaSemanal.hs
+++ lista_semanal/listaSemanal.hs
@@ -1,142 +1,118 @@
-
-{-Objetivo da aula 
-  exercícios e introdução à List Comprehension-}
-
 import Data.Char
+{--As funções f1_p1 e f2_p2, a seguir, são específicas. A partir
+de uma [String], elas geram [(Bool, String)] separando as strings
+que só possuem dígitos das demais --}
 
--- List comprehension---------------------------------------
-
-{-multiplica todos elementos pares da lista pela escalar -}
-f1 :: Int -> [Int] -> [Int]
-f1 x a = [x * y|y <- a, y `mod` 2 == 0 ]
-
-{-dado um booleano, decide por multiplicar todos elementos por 2 ou somar todos elementos com 5 -}
-f2 :: Bool -> [Int] -> [Int]
-f2 x a
-    |x = [2 * y|y <- a]
-    |otherwise = [5 + y|y <- a]
-
-{-fazer a função f0 que agrupa em duplas os cabeças de duas listas-}
-f0 ::[t]->[u]->[(t,u)]
-f0 [] [] = []
-f0 (a:b) (c:d) = [(a,c)] ++ f0 b d
+--questão 01
+f1_p1::String->Bool
+f1_p1 [] = True
+f1_p1 (x:xs)
+  |isDigit1 x = f1_p1 xs
+  |otherwise = False
 
 
-{-objetivo da computação dado em sala de aula:
-  Filtrar o resultado de produto cartesiano 
-  para gerar o resultado de f0-}  
-{- gera o produto cartesiano entre duas listas -}  
-
---f3::[t]->[u]->[(t,u)]
-
-
-
-{- desfaz a lista de duplas gerada por f3 -}
---f3_aux01 ::[(t,u)]-> ([t],[u])
+-- questão 02
+f2_p1::[String]->[(Bool, String)]
+f2_p1 [] = []
+f2_p1 (x:xs)
+  |f1_p1 x = [(True,x)] ++ f2_p1 xs
+  |otherwise = [(False, x)] ++ f2_p1 xs
 
 
-{-conta as ocorrências repetidas de fst(dupla) 
-  da lista gerada por f3 -}
---f3_count ::(Eq t)=>[(t,u)]-> Int
+isDigit1 x
+  |ord x >= 48 && ord x < 58 = True
+  |otherwise = False
 
+--caso o alpha seja qualquer coisa menos um int--
+isAlpha1 :: Char -> Bool
+isAlpha1 x
+  |ord x >= 48 && ord x < 58 = False
+  |otherwise = True
 
-{-solução que gera lista de listas de duplas separando as listas com duplas com mesmo fst-}
---f3_newList::(Eq u)=> u-> [(u,t)]->[(u,t)]
---f3_newList a l = [b|b<-l, a==fst b]
-
---f3_newListDel::(Eq u)=> u-> [(u,t)]->[(u,t)]
---f3_newListDel a l = [b|b<-l, a/=fst b]
-
---f3_newListofList::(Eq u)=>[(u,t)]->[[(u,t)]]
---f3_newListofList   []      = []
---f3_newListofList   (a:x)   = (f3_newList (fst a) (a:x)):f3_newListofList (f3_newListDel (fst a) (a:x))
-
-{- função que gera o filtro no produto cartesiano 
-   -------aqui, tem algo a ser ajustado---------}
-   
---f3_filtra::Int->[[(u,t)]]->[(u,t)]
---f3_filtra   _   [] = []
---f3_filtra     i (a:b)= busca i a:f3_filtra (i+1) b
-
---busca::Int->[(u,t)]->(u,t)
---busca i (a:b)
---  | i/=0      = busca (i-1) b
- -- | otherwise = a
+{-generalizando a função f2_p1 -}
+f2_p1_generica::(String->Bool)->[String]->[(Bool,String)]
+f2_p1_generica x s = [(x a,a)|a <- s]
   
+{- Mas, essas funções podem ser generalizadas.
+ Pensemos em uma solução recebe uma [String] e retorna uma [(Bool, String)]
+ em que o Bool é True se o String atende a uma característica determinada.
+ Para tanto, devemos implementar as características desejaveis -}
 
-------- Revisão -----------------------------------------------------
+cr01_Todos_Char::(Char->Bool)->String->Bool
+cr01_Todos_Char f [a]     = f a
+cr01_Todos_Char f (a:b)   = f (a) && cr01_Todos_Char f b
 
-{-01 função que retorna lista de duplas com char e posição na ASCII -}
-
-listaDuplaCharInt:: Int-> [(Char,Int)]
-listaDuplaCharInt 0 = []
-listaDuplaCharInt x = [(chr x, x)] ++ listaDuplaCharInt (x - 1)
-
-
-{-02 função meuChr que pesquisa um char pelo int na lista gerada -}
-
-meuChr :: Int -> Char
-meuChr x = meuChr_aux x (listaDuplaCharInt 126)
-
-meuChr_aux :: Int -> [(Char, Int)] -> Char
-meuChr_aux x (a:b)
-    |x == snd(a) = fst(a)
-    |otherwise = meuChr_aux x b
-
-{-03 função meuOrd que pesquisa o int pelo char na lista gerada -}
-meuOrd :: Char -> Int
-meuOrd x = meuOrd_aux x (listaDuplaCharInt 126)
-
-meuOrd_aux :: Char -> [(Char, Int)] -> Int
-meuOrd_aux x [] = error "Caracter nao encontrado"
-meuOrd_aux x (a:b)
-    |x == fst(a) = snd a
-    |otherwise = meuOrd_aux x b
-
-{-04 função que ordena uma lista de inteiros -}
-ordenaLista::[Int]->[Int]
-ordenaLista [] = []
-
-{-05 seja o tipo [(Bool, [Int])]. 
-Faça uma função que ordena [Int] quando o booleano é True. 
-Também, passe o Bool para False, quando ordenar [Int]
-exemplo: ordenaListaDupla [(True,[3,4,1,0,9]),(False,[]),(True,[4,3,2,1,0])]
-retorna:                  [(False,[0,1,3,4,9]),(False,[]),(False,[0,1,2,3,4])]
--}
-
-ordenaListaDupla::[(Bool, [Int])]->[(Bool, [Int])]
-ordenaListaDupla [] = []
+cr01_Algum_Char::(Char->Bool)->String->Bool
+cr01_Algum_Char f [a]     = f a
+cr01_Algum_Char f (a:b)   = f (a) || cr01_Algum_Char f b
 
 
+{-observe que todos ou alguns podem ser generalizados, também. 
+  Para isso, basta passar o operador lógico && ou || -}
+
+cr01_A_T_Char::(Bool->Bool->Bool)->(Char->Bool)->String->Bool  
+cr01_A_T_Char _ f [a]     = f a
+cr01_A_T_Char o f (a:b)   = (o) (f (a)) (cr01_A_T_Char o f b)
 
 
+{- as funções abaixo filtram de [(Bool, String)] a [String]
+   de acordo com o Booleano -}
+    
+{-como transformá-las em uma função genérica? -}
 
+filtraT::[(Bool, String)]->[String]
+filtraT [] = []
+filtraT (a:b)
+  |fst a     = snd a:filtraT b
+  |otherwise =       filtraT b
+  
+filtraF::[(Bool, String)]->[String]  
+filtraF [] = []  
+filtraF (a:b)
+  |not(fst a) = snd a:filtraF b
+  |otherwise  =       filtraF b
 
+{--------------------solução---------------------}
+filtraTF::(Bool->Bool)->[(Bool, String)]->[String]
+filtraTF _ [] = []
+filtraTF f (a:b)
+  |f (fst a)     = snd a:filtraTF f b
+  |otherwise =       filtraTF f b
 
+seletor  x = x
+inversor x = not(x)
+{------------------------------------------------}
 
+------------------  revisão e uso de alta ordem ----------------------------
+{- Considere f1 capaz de somar uma lista de inteiros se um Char for alfanumérico, 
+    ou multiplicar os elementos, caso contrário -}
 
+f1::Char->[Int]->Int
+f1 c x
+  |isDigit (c)     && x==[]    = 0
+  |not (isDigit c) && x==[]    = 1
+  |isDigit c                   = a + f1 c b
+  |otherwise                   = a * f1 c b
+    where (a:b) = x
 
+    
+{- reescreva f1 usando casamento de padrão -}
+--f2::Char->[Int]->Int
 
+{- reescreva f2 fazendo chamadas de funções para somar ou multiplicar -}
+--f3::Char->[Int]-> Int
 
+{- reescreva f3 usando função de alta ordem para definir o operador
+   Esta função é didática, pois mostra o uso de função de alta ordem
+   Neste caso, considere que a lista tem, pelo menos, um elemento -}
+ {- para lista de pelo menos um elemento -}
+--f4::(Int->Int->Int)->[Int]->Int
 
+{- faça a função myMap aplica uma função a cada elemento de uma lista -}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{- função que converte caixa baixa para caixa  alta
+   usar a função myMap para aplicar a uma String -}
+   
 
 
 
